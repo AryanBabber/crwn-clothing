eef6613a772ef5b9ac21691b1ea67cd08ecedff4
AryanBabber <aryanbabber12@gmail.com>
2023-01-28 23:24:03 +0530

    added categories within the shop page

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,16 +1,20 @@
-import { useContext } from "react";
-import './shop.styles.scss'
-import { ProductContext } from "../../context/product.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import { Routes, Route } from "react-router-dom";
+import CategoriesPreview from "../../components/categories-preview/categories-preview.component";
+import Category from "../category/category.component";
+import "./shop.styles.scss";
 
 const Shop = () => {
-    const {currentProduct} = useContext(ProductContext)
 	return (
-		<div className="products-container">
-			{currentProduct.map((product) => (
-                <ProductCard key={product.id} product={product} />
-			))}
-		</div>
+		<Routes>
+			<Route
+				index
+				element={<CategoriesPreview />}
+			/>
+			<Route
+				path=":category"
+				element={<Category />}
+			/>
+		</Routes>
 	);
 };
 
