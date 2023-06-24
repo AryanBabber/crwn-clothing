a81365ce0e43247d6cbca378d4dcf8868b61dabc
AryanBabber <aryanbabber12@gmail.com>
2023-06-24 15:14:42 +0530

    deleted config to make it private

--- src/utils/firebase/firebase.config.js
+++
@@ -1,8 +0,0 @@
-export const firebaseConfig = {
-	apiKey: "AIzaSyBDAVbbw_Nz_oQn0uUvC7hFqtNwROVtGC4",
-	authDomain: "crwn-clothing-db-487d4.firebaseapp.com",
-	projectId: "crwn-clothing-db-487d4",
-	storageBucket: "crwn-clothing-db-487d4.appspot.com",
-	messagingSenderId: "546895984921",
-	appId: "1:546895984921:web:6d7e1ea6955cd943b96d88",
-};
